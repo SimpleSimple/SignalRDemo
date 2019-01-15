320712788ea39dbf46f0951d2783b05f6eb9ef2a
playitouch@163.com <playitouch@163.com>
2019-01-16 01:06:38 +0800

    add

--- SignalrApplication/Hubs/MyHub.cs
+++ SignalrApplication/Hubs/MyHub.cs
@@ -2,15 +2,46 @@
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Diagnostics;
 
 namespace SignalRApplication
 {
     public class MyHub : Hub
     {
-        [HubMethodName("Hello")]
-        public void Hello()
+
+        public override System.Threading.Tasks.Task OnConnected()
         {
-            Clients.All.hello();
+            return base.OnConnected();
+        }
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            if (stopCalled) {
+                Trace.WriteLine(string.Format("ConnectionId {0 disconnected", Context.ConnectionId));
+            }
+            else {
+                Trace.WriteLine(string.Format("ConnectionId {0} time out", Context.ConnectionId));
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        public void Connect(string who)
+        {
+            Clients.All.notifyUserConnected(who);
+        }
+
+        [HubMethodName("broadcastMessage")]
+        public void BroadcastMessage(string name, string msg)
+        {
+            Clients.All.broadcastMessage(name, msg);
         }
     }
 }
